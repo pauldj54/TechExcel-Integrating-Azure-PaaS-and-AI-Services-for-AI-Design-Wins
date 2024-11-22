97514a98d7c7642f9ea1b5ac20722c8f9ef9bee3
Paul Vicente Hernandez Moreno <paul.pauldj54@gmail.com>
2024-11-22 11:42:35 +0100

    git ignore editet

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -79,7 +79,7 @@ app.UseHttpsRedirection();
 // This endpoint serves as the default landing page for the API.
 app.MapGet("/", async () => 
 {
-    return "Welcome to the Contoso Suites Web API!";
+    return "Welcome to the Contoso Suites Web API o yeah!";
 })
     .WithName("Index")
     .WithOpenApi();
